0cf02ed3074bbe61fb74937e6c4ae0947aaa0d91
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2021-01-01 19:51:52 -0600

    server running on PORT 8000

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('hello')
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+const express = require("express");
+const morgan = require("morgan");
+const MOVIEDEX = require("./moviedex.json");
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use((req, res) => {
+  res.send("Hello, world!");
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
