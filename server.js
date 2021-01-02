3683fca2a0ddeef4110ece80ae0b95b60a449bae
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2021-01-01 22:38:51 -0600

    validate bearer

--- server.js
+++ server.js
@@ -5,18 +5,33 @@ const express = require("express");
 const morgan = require("morgan");
 const MOVIEDEX = require("./moviedex.json");
 const app = express();
-
 app.use(morgan("dev"));
+app.use(function validateBearer(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  console.log("validate bearer token middlewear");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
 
 console.log(`API_TOKEN is ${process.env.API_TOKEN}`);
 
 const validGenre = [`Animation`, `Drama`, `Comedy`, `Romantic`];
-const handleGetTypes = (req, res) => {
+
+const handleGetGenres = (req, res) => {
   res.json(validGenre);
 };
 
-app.get("/genre", handleGetTypes);
+const handleGetCountry = (req, res) => {
+  res.send("welcome to the country!");
+};
 
-app.listen(8000, () => {
-  console.log(`Server listening at http://localhost:${8000}`);
+app.get("/genre", handleGetGenres);
+app.get("/country", handleGetCountry);
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
