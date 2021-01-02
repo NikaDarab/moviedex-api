637651cc14a3ce11e850c018c4ba1944de5b7aed
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2021-01-01 23:58:58 -0600

    handleGetMovie

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const MOVIEDEX = require("./moviedex.json");
 const app = express();
 app.use(morgan("dev"));
+
 app.use(function validateBearer(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
@@ -18,23 +19,25 @@ app.use(function validateBearer(req, res, next) {
 
 console.log(`API_TOKEN is ${process.env.API_TOKEN}`);
 
-const validGenre = [`Animation`, `Drama`, `Comedy`, `Romantic`];
-
-const handleGetGenres = (req, res) => {
-  res.json(validGenre);
-};
-
-const handleGetCountry = (req, res) => {
-  res.send("the list of the movies based on the country");
-};
-
-const handleAverageVote = (req, res) => {
-  res.send("the list of the movies based on the average vote");
-};
-
-app.get("/genre", handleGetGenres);
-app.get("/country", handleGetCountry);
-app.get("/vote"), handleAverageVote;
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIEDEX["movie_title"];
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.includes(req.query.genre)
+    );
+  }
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.genre.includes(req.query.country)
+    );
+  }
+  if (req.query["avg_vote"]) {
+    response = response.filter((movie) =>
+      movie["avg_vote"].includes(req.query.vote)
+    );
+  }
+  res.json(response);
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
